Fixed normalisation bug

CS1/OW/CS1_Model.py
@@ -217,17 +217,12 @@ class reactor_class(gym.Env):
    
       # Ks[3] = (Ks[3])*13 + 290
       self.info = {'Ks':Ks_norm}
-      if self.PID_pos:
-        if self.i == 0:
-            u  = PID(Ks_norm, state[0:2], x_sp, np.array([[0,0]]))
-        else:
-            u  = PID(Ks_norm,state[0:2], x_sp, np.array(self.e_history))
 
       if self.PID_vel:
         if self.i < 2:
           u = 295
         else:
-          u =  PID_velocity(Ks,state[0:2], x_sp, np.array(self.e_history),self.u_history,self.ts,np.array(self.s_history))
+          u =  PID_velocity(Ks_norm,state[0:2], x_sp, np.array(self.e_history),self.u_history,self.ts,np.array(self.s_history))
     if self.DS:
       u = self.u_DS
     # simulate system

CS1/OW/CS1_PID_Comp.py
@@ -54,12 +54,13 @@ def rollout(Ks, PID_Form,opt,reps):
       Tc_eval[i,r_i] = env.u_history[-1]
     r_eval[:,r_i] = r_tot
   r = -1*np.mean(r_tot)
-  
+  ISE = np.sum((Ca_des - np.median(Ca_eval,axis=1))**2)
 
   if opt:
     return r
   else:
     print(r)
+    print(ISE,'ISE')
     return Ca_eval, T_eval, Tc_eval, ks_eval
 
 
@@ -97,24 +98,24 @@ def plot_simulation_comp(Ca_dat_PG, T_dat_PG, Tc_dat_PG,ks_eval_PG,SP,ns):
   plt.show()
 
  
-  fig, axs = plt.subplots(2, 1, figsize=(10, 12))
+  fig, axs = plt.subplots(3, 1, figsize=(10, 12))
   axs[0].set_title('velocity form PID Parameters')
 
   axs[0].step(t, np.median(ks_eval_PG[0,:,:],axis=1), col[0],where = 'post', lw=1,label = labels[0])
-    # plt.gca().fill_between(t, np.min(ks_eval_PG[ks_i,:,:],axis=1), np.max(ks_eval_PG[ks_i,:,:],axis=1),color=col_fill[ks_i], alpha=0.2)
+  axs[0].fill_between(t, np.min(ks_eval_PG[0,:,:],axis=1), np.max(ks_eval_PG[0,:,:],axis=1),color=col_fill[0], alpha=0.2)
                             
   axs[0].set_ylabel('Ca PID Parameter (velocity form)')
   axs[0].set_xlabel('Time (min)')
   axs[0].legend(loc='best')
   axs[0].set_xlim(min(t), max(t))
   for ks_i in range(1,3):
-    axs[1].step(t, np.median(ks_eval_PG[ks_i,:,:],axis=1), col[ks_i],where = 'post', lw=1,label = labels[ks_i])
-    # plt.gca().fill_between(t, np.min(ks_eval_PG[ks_i,:,:],axis=1), np.max(ks_eval_PG[ks_i,:,:],axis=1),color=col_fill[ks_i], alpha=0.2)
+    axs[ks_i].step(t, np.median(ks_eval_PG[ks_i,:,:],axis=1), col[ks_i],where = 'post', lw=1,label = labels[ks_i])
+    axs[ks_i].fill_between(t, np.min(ks_eval_PG[ks_i,:,:],axis=1), np.max(ks_eval_PG[ks_i,:,:],axis=1),color=col_fill[ks_i], alpha=0.2)
                             
-  axs[1].set_ylabel('Ca PID Parameter (velocity form)')
-  axs[1].set_xlabel('Time (min)')
-  axs[1].legend(loc='best')
-  axs[1].set_xlim(min(t), max(t))
+    axs[ks_i].set_ylabel('Ca PID Parameter (velocity form)')
+    axs[ks_i].set_xlabel('Time (min)')
+    axs[ks_i].legend(loc='best')
+    axs[ks_i].set_xlim(min(t), max(t))
   plt.tight_layout()
   # plt.savefig('velocity_vs_pos_ks_vel.pdf')
   plt.show()
@@ -132,15 +133,16 @@ x0_norm = (((x0-x_norm[0]) / (x_norm[1] - x_norm[0]))*2 - 1).flatten()
 
 #x0_norm = np.tile(x0_norm, (popsize, 1))
 
-# def save_current_state(intermediate_result):
-#     np.save('current_solution.npy', intermediate_result.x)
-#     with open('current_function_value.txt', 'w') as f:
-#         f.write(str(intermediate_result.fun))
-# print('Starting Velocity Opt')
-result_vel =  differential_evolution(rollout, polish= False,popsize = 3, bounds=bounds, args= ('vel', True,3), maxiter = 5000,disp = True)
+def save_current_state(x, convergence):
+    np.save('current_solution.npy', x)
+    with open('current_function_value.txt', 'w') as f:
+        f.write(str(convergence))
+print('Starting Velocity Opt')
+result_vel =  differential_evolution(rollout, polish = False,popsize = 3, bounds=bounds, args= ('vel', True,3), maxiter = 5000,disp = True)
 np.save('GS_Global_vel.npy', result_vel.x)
 # Ks_vel = result_vel.x
-# Ks_vel = np.load('GS_Global_vel.npy')
+# Ks_vel = np.load('current_solution (7).npy')
+# Ks_vel = np.load('GS_Global_vel_const.npy')
 # np.save('GS_Global_vel_const.npy',Ks_vel)
 # Ks_vel = np.load('GS_Global_vel_reducedPop.npy')
 # Ks_pos = np.load('GS_Global_pos_const.npy')
@@ -155,4 +157,3 @@ np.save('GS_Global_vel.npy', result_vel.x)
 # Ca_dat_vel, T_dat_vel, Tc_dat_vel, ks_eval_vel = rollout(Ks_vel, 'vel', opt = False,reps = 10)
 
 # plot_simulation_comp(Ca_dat_vel, T_dat_vel, Tc_dat_vel,ks_eval_vel,SP,ns)
-

CS3/CS3_Training.py
@@ -11,8 +11,8 @@ from torch_pso import ParticleSwarmOptimizer
 import copy
 from scipy.optimize import differential_evolution
 # Create Environment
-ns = 600
-env = RSR(ns, test=False, plot=False)
+ns = 300
+env = RSR(ns, test=True, plot=False)
 
 
 def sample_uniform_params(params_prev, param_max, param_min):
@@ -193,8 +193,8 @@ def rollout_DFO(ns,policy,reps,test):
             tot_reward += reward
             i += 1
         rewards[:,r_i] = tot_reward 
-        if test:
-            print(tot_reward)
+        # if test:
+        #     print(tot_reward)
     return states, actions, tot_reward,controls
 
 class Net(torch.nn.Module):
@@ -239,7 +239,7 @@ class Net(torch.nn.Module):
     return y,None
   
 def criterion(policy,ns):
-  s,a,r,c = rollout_DFO(ns,policy,1,False)
+  s,a,r,c = rollout_DFO(ns,policy,1,True)
   r = np.array(r)
   r_tot = -1*np.sum(r)
   
@@ -261,7 +261,7 @@ def criterion(policy,ns):
 # new_swarm = 0
 # tol = 0.01
 
-# env = RSR(ns,test = False,plot= False)
+# env = RSR(ns,test = True,plot= False)
 
 # max_iter = 30
 
@@ -324,16 +324,16 @@ def criterion(policy,ns):
 # print('Saving Best Policy...')
 # torch.save(best_policy.state_dict(), 'DFO_SP_0703')
 # best_policy_sd = best_policy.state_dict()
-best_policy_sd = torch.load('DFO_SP_0703.pth')
-policy_plot = Net(n_fc1 = 256,n_fc2 = 256,activation = torch.nn.ReLU,n_layers = 1,deterministic = True) # Deterministic for plotting
-policy_plot.load_state_dict(best_policy_sd)
-print('Plotting Best Policy...')
+# # best_policy_sd = torch.load('DFO_SP_0703.pth')
+# policy_plot = Net(n_fc1 = 256,n_fc2 = 256,activation = torch.nn.ReLU,n_layers = 1,deterministic = True) # Deterministic for plotting
+# policy_plot.load_state_dict(best_policy_sd)
+# print('Plotting Best Policy...')
 
-env = RSR(ns,test=True,plot=False)
-s,a,r,c = rollout_DFO(ns,policy_plot,10,True)
+# env = RSR(ns,test=True,plot=False)
+# s,a,r,c = rollout_DFO(ns,policy_plot,10,True)
 
 
-plot_simulation(s,a,c,ns)
+# plot_simulation(s,a,c,ns)
 
 # Ks = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 # s,a,r,c = rollout_test(ns,Ks)
@@ -383,12 +383,12 @@ plot_simulation(s,a,c,ns)
 # plot_simulation(s, a, c, ns)
 
 
-# bounds = [(-1,1)]*12
-# result_vel =  differential_evolution(rollout_test,polish = False, popsize= 1,bounds=bounds,args= (ns, True),maxiter = 100,disp = True)
+bounds = [(-1,1)]*12
+result_vel =  differential_evolution(rollout_test,polish = False, popsize= 1,bounds=bounds,args= (ns, True),maxiter = 100,disp = True)
 
-# np.save('GS_cons.npy',result_vel.x)
-# Ks = result_vel.x
-# Ks = np.load('GS_cons.npy')
-# s,a,r,c = rollout_test(Ks,ns,False)
+np.save('GS_cons.npy',result_vel.x)
+Ks = result_vel.x
+Ks = np.load('GS_cons.npy')
+s,a,r,c = rollout_test(Ks,ns,False)
 
-# plot_simulation(s,a,c,ns)
+plot_simulation(s,a,c,ns)

CS3/RSR_Model_1602.py
@@ -31,14 +31,14 @@ class RSR(gym.Env):
     # Action Space
     self.action_space = spaces.Box(low = -1, high = 1, shape = (12,))
     self.action_space_unnorm = spaces.Box(low = np.array([8,8,0.67,8]), high = np.array([47,47,3,45]))
-    self.PID_space = spaces.Box(low = np.array([8,2,0,8,2,0.1,9,0,0.1,12,0,0]),
-                                high = np.array([14,8,0.4,14,8,0.4,10,5,0.4,18,5,0.4]))
+    self.PID_space = spaces.Box(low = np.array([0,2,0,  8,2,0.1,  9,0,0.1,  0,0,0]),
+                                high = np.array([14,8,0.4,  20,8,0.4,  10,5,0.4,  18,5,0.4]))
     
     self.SP_test = np.array([[22 for i in range(int(ns/3))] + [20.5 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))],[22 for i in range(int(ns/3))] + [20.5 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))],[22 for i in range(int(ns/3))] + [20.5 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))]])
     
-    self.SP1 = np.array([[21.5 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[21.5 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[21.5 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))]])
-    self.SP2 = np.array([[21 for i in range(int(ns/3))] + [22 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[21 for i in range(int(ns/3))] + [22 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[21 for i in range(int(ns/3))] + [22 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))]])
-    self.SP3 =  np.array([[20.5 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[20 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))],[20 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[21.5 for i in range(int(ns/3))]])
+    self.SP1 = self.SP_test#np.array([[22.1 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19 for i in range(int(ns/3))],[22.1 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19 for i in range(int(ns/3))],[22.1 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19 for i in range(int(ns/3))]])
+    self.SP2 = self.SP_test#np.array([[21.9 for i in range(int(ns/3))] + [20 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))],[21.9 for i in range(int(ns/3))] + [20 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))],[21.9 for i in range(int(ns/3))] + [20 for i in range(int(ns/3))]+[19.5 for i in range(int(ns/3))]])
+    self.SP3 =  self.SP_test#np.array([[22 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19.2 for i in range(int(ns/3))],[22 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19.2 for i in range(int(ns/3))],[22 for i in range(int(ns/3))] + [21 for i in range(int(ns/3))]+[19.2 for i in range(int(ns/3))]])
     if self.test:
       self.x0 = copy.deepcopy(np.array([21.5, 0.8861, 0.1082, 0.0058, 21.5, 0.8861, 0.1082, 0.0058, 21.5, 0.1139, 0.7779, 0.1082,self.SP_test[0,0],self.SP_test[1,0],self.SP_test[2,0]]))
       self.SP  = np.array([self.SP_test])