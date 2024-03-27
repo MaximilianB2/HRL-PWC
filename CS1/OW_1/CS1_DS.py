d72a4844184ee961363b5a46e133fec105acaf2c
Max Bloor <max.bloor@gmail.com>
2024-03-27 12:10:39 +0000

    2x2 System implement with DS Analysis

--- CS1/OW_1/CS1_DS.py
+++ CS1/OW_1/CS1_DS.py
@@ -6,36 +6,70 @@ import copy
 import torch
 import torch.nn.functional as F
 from scipy.optimize import minimize
-
-Tc_n = 102
-Tc_vec = np.linspace(290,445,Tc_n)
-ns = 240
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+n = 20
+Tc_vec = np.linspace(290,450,n)
+F_vec = np.linspace(99,103,n)
+ns = 120
 env = reactor_class(test = True, ns=ns, DS= True)
 
-CV = np.zeros([Tc_vec.shape[0],2])
+x_b = np.zeros((n,n))
+V = np.zeros_like(x_b)
+# for j, F_j in enumerate(F_vec):
+#   for i,Tc in enumerate(Tc_vec):
+#     env.reset()
+#     for ns_i in range(ns-1):
+#       u = np.array([Tc,F_j])
+#       s_norm, r, done, info,_  = env.step(u)
+#       s = s_norm*(env.observation_space.high - env.observation_space.low) + env.observation_space.low
 
+#     x_b[i,j] = env.state[1]
+#     x_b[x_b < 0] = 0
+#     V[i,j] = env.state[4]
+
+# fig, ax = plt.subplots(2,figsize=(10,10))
+# plt.subplots_adjust(hspace=0.5, wspace=0.5)
+
+# fun = ax[0].contourf(F_vec, Tc_vec, x_b, cmap='Spectral_r', alpha=0.8)
+# divider = make_axes_locatable(ax[0])
+# cax = divider.append_axes('right', size='5%', pad=0.1)
+# cbar = fig.colorbar(fun, cax=cax, orientation='vertical')
+# ax[0].set_ylabel('Temperature')
+# ax[0].set_xlabel('Flow in')
+# ax[0].set_title('Mol Fraction of B')
+
+# fun = ax[1].contourf(F_vec, Tc_vec, V, cmap='Spectral_r', alpha=0.8)
+# divider = make_axes_locatable(ax[1])
+# cax = divider.append_axes('right', size='5%', pad=0.1)
+# fig.colorbar(fun, cax=cax, orientation='vertical')
+# ax[1].set_ylabel('Temperature')
+# ax[1].set_xlabel('Flow in')
+# plt.savefig('CS1_2x2_DS.pdf')
+# plt.show()
+
+
+
+F_j = 100
+x_b_slice = np.zeros(n)
+Tc_vec = np.linspace(290,450,n)
 for i,Tc in enumerate(Tc_vec):
- 
   env.reset()
   for ns_i in range(ns-1):
-    s_norm, r, done, info,_  = env.step(Tc)
+    u = np.array([Tc,F_j])
+    s_norm, r, done, info,_  = env.step(u)
     s = s_norm*(env.observation_space.high - env.observation_space.low) + env.observation_space.low
- 
-  CV[i,:] = env.state[1]
-y = np.linspace(1,1,Tc_n)
-# print(CV)
-SP = [0.85 for i in range(int(Tc_n/3))] + [0.4 for i in range(int(Tc_n/3))] + [0.1 for i in range(int(Tc_n/3))]
+  x_b_slice[i] = env.state[1]
+  x_b_slice[x_b_slice < 0] = 0
+
+
+
+
 plt.figure()
-plt.plot(Tc_vec, CV[:,0], color='tab:blue', label = 'Equilibrium Concentration')
-
-# plt.fill_between(np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]), y, where=(np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]) > 21) & (np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]) < 28), color='tab:red', alpha=0.4, label="Unstable", edgecolor=None)
-# plt.fill_between(np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]), y, where=(np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]) <= 22) | (np.linspace(0,Tc_vec.shape[0],Tc_vec.shape[0]) >= 27), color='tab:green', alpha=0.4, label="Stable", edgecolor=None)
-
-plt.ylabel('Concentration of species B')
-plt.xlabel('Cooling Temperature (K)')
-plt.ylim(0,1)
-plt.grid(True)
-plt.legend()
+plt.plot(Tc_vec,x_b_slice,color = 'tab:blue')
 plt.xlim(np.min(Tc_vec),np.max(Tc_vec))
-plt.savefig('CS1_DS.pdf')
+plt.xlabel('Cooling Temperature')
+plt.ylabel('Concentration of species B')
+plt.grid(True)
+plt.title('Slice at F = 100m3/min')
+plt.savefig('cs1_2x2_slice.pdf')
 plt.show()
--- CS1/OW_1/CS1_Model.py
+++ CS1/OW_1/CS1_Model.py
@@ -24,59 +24,76 @@ def PID(Ks, x, x_setpoint, e_history):
 
 
     return u
-def PID_velocity(Ks,x,x_setpoint,e_history,u_prev,ts,s_hist):
+def PID_velocity(Ks,e,e_history,u_prev,ts,s_hist):
     # K gains
     dt = ts[1] - ts[0]
     KpCa = Ks[0]; KiCa = Ks[1] + 1e-6; KdCa = Ks[2]
-    e = x_setpoint - x
+    KpF = Ks[3]; KiF = Ks[4] + 1e-6; KdF = Ks[5] 
+   
    
     
-    u = u_prev[-1] + KpCa*(e[1] - e_history[-1,1]) + (KpCa/KiCa)*e[1]*dt - KpCa*KdCa*(e[1]-2*e_history[-1,1]+e_history[-2,1])/dt
+    Tc = u_prev[-1,0] + KpCa*(e[0] - e_history[-1,0]) + (KpCa/KiCa)*e[0]*dt - KpCa*KdCa*(e[0]-2*e_history[-1,0]+e_history[-2,0])/dt
     
-    u = min(max(u,290),450)
+    Tc = min(max(Tc,290),450)
+
+    F = u_prev[-1,1] + KpF*(e[1] - e_history[-1,1]) + (KpF/KiF)*e[1]*dt - KpF*KdF*(e[1]-2*e_history[-1,1]+e_history[-2,1])/dt
+
+    F = min(max(F,97),105)
+
+    u = np.array([Tc,F])
     return u
 
 
 def cstr_CS1(x,t,u,Tf,Caf,k0,UA):
 
-    # ==  Inputs (3) == #
-    Tc = u # Temperature of cooling jacket (K)
-    # Tf = Feed Temperature (K)
-    # Caf = Feed Concentration (mol/m^3)
+    # ==  Inputs (2) == #
+    Tc  = u[0] # Temperature of Cooling Jacket (K)
+    Fin = u[1] # Volumetric Flowrate at inlet (m^3/sec) = 100
 
-    # == States == #
+    # == States (5) == #
     Ca = x[0] # Concentration of A in CSTR (mol/m^3)
     Cb = x[1] # Concentration of B in CSTR (mol/m^3)
     Cc = x[2] # Concentration of C in CSTR (mol/m^3)
     T  = x[3] # Temperature in CSTR (K)
+    V  = x[4] # Volume in CSTR (K)
 
     # == Process parameters == #
-    q      = 100    # Volumetric Flowrate (m^3/sec)
-    V      = 100    # Volume of CSTR (m^3)
-    rho    = 1000   # Density of A-B Mixture (kg/m^3)
-    Cp     = 0.239  # Heat capacity of A-B Mixture (J/kg-K)
-    mdelH  = 5e3    # Heat of reaction for A->B (J/mol)
-    EoverR = 8750   # E -Activation energy (J/mol), R -Constant = 8.31451 J/mol-1
+    Tf       = 350    # Feed Temperature (K)
+    Caf      = 1      # Feed Concentration of A (mol/m^3)
+    Fout     = 100    # Volumetric Flowrate at outlet (m^3/sec)
+    #V       = 100    # Volume of CSTR (m^3)
+    rho      = 1000   # Density of A-B Mixture (kg/m^3)
+    Cp       = 0.239  # Heat Capacity of A-B-C Mixture (J/kg-K)
+    UA       = 5e4    # U -Heat Transfer Coefficient (W/m^2-K) A -Area - (m^2)
+    # Reaction A->B
+    mdelH_AB  = 5e3    # Heat of Reaction for A->B (J/mol)
+    EoverR_AB = 8750   # E -Activation Energy (J/mol), R -Constant = 8.31451 J/mol-K
+    k0_AB     = 7.2e10 # Pre-exponential Factor for A->B (1/sec)#
+    rA        = k0_AB*np.exp(-EoverR_AB/T)*Ca # reaction rate
+    # Reaction B->C
     mdelH_BC  = 4e3      # Heat of Reaction for B->C (J/mol) => 5e4
     EoverR_BC = 10750    # E -Activation Energy (J/mol), R -Constant = 8.31451 J/mol-K !! 10
-    k0_BC     = 8.2e10    # Pre-exponential Factor for A->B (1/sec)# !! 8
-    rA          =    k0*np.exp(-EoverR/T)*Ca # reaction rate
-    rB        = k0_BC*np.exp(-EoverR_BC/T)*Cb
-    dCadt  = q/V*(Caf - Ca) - rA     # Calculate concentratioing n derivative
-    dCbdt    = rA - rB - q*Cb/V         # B Concentration Derivative
-    dCcdt    = rB      - q*Cc/V         # B Concentration Derivative
-    dTdt   = q/V*(Tf - T) \
-              + mdelH/(rho*Cp)*rA \
+    k0_BC     = 8.2e10   # Pre-exponential Factor for A->B (1/sec)# !! 8
+    rB        = k0_BC*np.exp(-EoverR_BC/T)*Cb # reaction rate !! **2
+    # play with mdelH_BC, factor on Cb**2 and k0_BC, maybe even EoverR_BC
+
+    # == Concentration Derivatives == #
+    dCadt    = (Fin*Caf - Fout*Ca)/V - rA  # A Concentration Derivative
+    dCbdt    = rA - rB - Fout*Cb/V         # B Concentration Derivative
+    dCcdt    = rB      - Fout*Cc/V         # B Concentration Derivative
+    dTdt     = Fin/V*(Tf - T) \
+              + mdelH_AB/(rho*Cp)*rA \
               + mdelH_BC/(rho*Cp)*rB \
               + UA/V/rho/Cp*(Tc-T)   # Calculate temperature derivative
+    dVdt     = Fin - Fout
 
     # == Return xdot == #
-    xdot    = np.zeros(4)
+    xdot    = np.zeros(5)
     xdot[0] = dCadt
     xdot[1] = dCbdt
     xdot[2] = dCcdt
     xdot[3] = dTdt
-    
+    xdot[4] = dVdt
     return xdot
 
 
@@ -100,17 +117,19 @@ class reactor_class(gym.Env):
     if self.test:
       #Ca_des1 = [0.05 for i in range(int(ns/3))] + [0.15 for i in range(int(ns/3))] + [0.25 for i in range(int(ns/3))]
       Ca_des1 = [0.3 for i in range(int(ns/3))] + [0.45 for i in range(int(ns/3))] + [0.6 for i in range(int(ns/3))]
+      V_des = [100 for i in range(int(ns))]
     
       
     
-    self.observation_space = spaces.Box(low = np.array([.70, 310,.70, 310,0.7]),high= np.array([1,340,1,340,1]))
+    self.observation_space = spaces.Box(low = np.array([.70, 310,90,.70, 310,90,0.7]),high= np.array([1,340,200,1,340,200,1]))
     self.action_space = spaces.Box(low = np.array([-1]*3),high= np.array([1]*3))
 
     
-    self.SP = np.array(([Ca_des1],[Ca_des2],[Ca_des3]),dtype = object)
+    self.SP = np.array(([[Ca_des1],[Ca_des2],[Ca_des3]],[[V_des]]),dtype = object)
 
     self.Ca_ss = 0.80
     self.T_ss  = 327
+    self.V_ss = 100
     self.x0    = np.empty(2)
     self.x0[0] = self.Ca_ss
     self.x0[1] = self.T_ss
@@ -159,8 +178,9 @@ class reactor_class(gym.Env):
     elif self.robust_test:
       self.UA = self.UA_dist_test()
       self.k0 = self.k0_dist_test()
-    Ca_des = self.SP[self.SP_i,0][self.i]
-    self.state = np.array([self.Ca_ss,0,0,self.T_ss,self.Ca_ss,0,0,self.T_ss,Ca_des])
+    Ca_des = self.SP[0][self.SP_i][0][0]
+    V_des = self.SP[1][0][0][0]
+    self.state = np.array([self.Ca_ss,0,0,self.T_ss,self.V_ss,self.Ca_ss,0,0,self.T_ss,self.V_ss,Ca_des,V_des])
     self.done = False
     if not self.test:
       self.disturb = False
@@ -168,7 +188,7 @@ class reactor_class(gym.Env):
     self.e_history = []
     self.s_history = []
     self.ts = [self.t[self.i],self.t[self.i+1]]
-    self.RL_state = [self.state[i] for i in [1,3,5,7,8]]
+    self.RL_state = [self.state[i] for i in [1,3,4,6,8,9,10]]
     self.state_norm = (self.RL_state -self.observation_space.low)/(self.observation_space.high - self.observation_space.low)
     return self.state_norm,{}
 
@@ -178,10 +198,10 @@ class reactor_class(gym.Env):
        self.info = {}
     if self.i % 5 == 0:
       self.action = copy.deepcopy(action_policy)
-    Ca_des = self.SP[self.SP_i,0][self.i]
-   
+    Ca_des = self.SP[0][self.SP_i][0][self.i]
+    V_des = self.SP[1][0][0][self.i]
     
-    self.state,rew = self.reactor(self.state,self.action,Ca_des)
+    self.state,rew = self.reactor(self.state,self.action,Ca_des,V_des)
     self.i += 1
     if self.i == self.ns:
         if self.test:
@@ -189,17 +209,19 @@ class reactor_class(gym.Env):
         elif self.SP_i < 2:
           self.SP_i += 1
           self.i = 0
-          self.state = np.array([self.Ca_ss,0,0,self.T_ss,self.Ca_ss,0,0,self.T_ss,Ca_des])
+          Ca_des = self.SP[0][self.SP_i][0][0]
+          V_des = self.SP[1][0][0][0]
+          self.state = np.array([self.Ca_ss,0,0,self.T_ss,self.V_ss,self.Ca_ss,0,0,self.T_ss,self.V_ss,Ca_des,V_des])
           self.u_history = []
           self.e_history = []
         else:
           self.done = True
 
-    self.RL_state = [self.state[i] for i in [1,3,5,7,8]]
+    self.RL_state = [self.state[i] for i in [1,3,4,6,8,9,10]]
     self.state_norm = (self.RL_state -self.observation_space.low)/(self.observation_space.high - self.observation_space.low)
     return self.state_norm,rew,self.done,False,self.info
 
-  def reactor(self,state,action,Ca_des):
+  def reactor(self,state,action,Ca_des,V_des):
     if not self.DR or not self.robust_test:
       k0 = 7.2e10 #1/sec
       UA = 7e5 # W/K
@@ -213,8 +235,12 @@ class reactor_class(gym.Env):
     Cb = state[1]
     Cc = state[2]
     T  = state[3]
-  
-    x_sp    = np.array([Ca_des])
+    V  = state[4]
+
+    x_sp    = np.array([Ca_des,V_des])
+    e = np.zeros(2)
+    e[0]  = x_sp[0] - state[1]
+    e[1]  = x_sp[1] - state[4]
     
     
     Ks = copy.deepcopy(action) #Ca, T, u, Ca setpoint and T setpoint
@@ -229,35 +255,37 @@ class reactor_class(gym.Env):
 
       if self.PID_vel:
         if self.i < 2:
-          u = 302
+          u = np.array([302,100])
         else:
-          u =  PID_velocity(Ks_norm,state[0:2], x_sp, np.array(self.e_history),self.u_history,self.ts,np.array(self.s_history))
+          u =  PID_velocity(Ks_norm,e,np.array(self.e_history),self.u_history,self.ts,np.array(self.s_history))
     if self.DS:
       u = self.u_DS
     # simulate system
 
-    y       = odeint(cstr_CS1,state[0:4],self.ts,args=(u,self.Tf,self.Caf,k0,UA))
+    y       = odeint(cstr_CS1,state[0:5],self.ts,args=(u,self.Tf,self.Caf,k0,UA))
 
     # add process disturbance
 
-    Ca_plus = y[-1][0] + np.random.uniform(low=-0.00075,high=0.00075)
-    Cb_plus = y[-1][1] + np.random.uniform(low=-0.00075,high=0.00075)
-    Cc_plus = y[-1][2] + np.random.uniform(low=-0.00075,high=0.00075)
-    T_plus  = y[-1][3] + np.random.uniform(low=-.025,high=0.025)
+    Ca_plus = y[-1][0] #+ np.random.uniform(low=-0.00075,high=0.00075)
+    Cb_plus = y[-1][1] #+ np.random.uniform(low=-0.00075,high=0.00075)
+    Cc_plus = y[-1][2] #+ np.random.uniform(low=-0.00075,high=0.00075)
+    T_plus  = y[-1][3] #+ np.random.uniform(low=-.025,high=0.025)
+    V_plus = y[-1][4] #+ np.random.uniform(low=-.025,high=0.025)
     # collect data
-    state_plus = np.zeros(9)
+    state_plus = np.zeros(11)
     state_plus[0]   = Ca_plus
     state_plus[1]   = Cb_plus
     state_plus[2]   = Cc_plus
     state_plus[3]   = T_plus
-    state_plus[4]   = Ca
-    state_plus[5]   = Cb
-    state_plus[6]   = Cc
-    state_plus[7]   = T
-    state_plus[8]   = Ca_des
+    state_plus[4] = V_plus
+    state_plus[5]   = Ca
+    state_plus[6]   = Cb
+    state_plus[7]   = Cc
+    state_plus[8]   = T
+    state_plus[9]   = V
+    state_plus[10]   = Ca_des
     # compute tracking error
-    e  = x_sp-state[0:2]
-    self.e_history.append((x_sp-state[0:2]))
+    self.e_history.append((e))
     #Penalise control action magnitude
     u_mag = np.abs(np.array(u-290))/10 #295 is lower bound of jacket temperature
     u_mag = u_mag/10
@@ -272,6 +300,6 @@ class reactor_class(gym.Env):
     self.s_history.append(state[0:2])
     #Compute reward (squared distance + scaled)
   
-    r_x = ((e[1])**2) * 1e2 #+ u_mag + u_cha
-
+    r_x = ((e[0])**2) * 1e2 #+ u_mag + u_cha
+    r_x += ((e[1])**2) 
     return state_plus, -r_x
